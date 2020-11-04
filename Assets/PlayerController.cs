17f5e4c9ce8683fe33e2c1f937ab24ead0bcff47
Devon Gregory <Devonlg98@gmail.com>
2020-11-03 17:20:45 -0800

    added skeletal animations and gui that interacts with it

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+    public float speed = 0.5f;
+    public Slider textSlider;
+    public float turnTime = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //textSlider = GetComponent<ShowValue>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        speed = textSlider.value * 2f;
+        if (turnTime > Time.time)
+        {
+            transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
+        }
+        else
+        {
+            transform.rotation *= Quaternion.Euler(0, 180f, 0);
+            turnTime += 10f;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        anim.SetFloat("speed", speed);
+    }
+}
---
+++ Assets/ShowValue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValue : MonoBehaviour
+{
+    Text PercentText;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PercentText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void textUpdate (float value)
+    {
+        PercentText.text = Mathf.RoundToInt(value * 100) + "%";
+    }
+}
